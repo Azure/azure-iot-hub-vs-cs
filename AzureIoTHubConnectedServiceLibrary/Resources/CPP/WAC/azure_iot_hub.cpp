39142c2310eeeebbfcb0f63b3202872ce96d0d2e
Artur Laksberg <arturl@microsoft.com>
2016-02-09 15:00:59 -0800

    receive_cloud_to_device_message for C++ UWP

--- AzureIoTHubConnectedServiceLibrary/Resources/CPP/WAC/azure_iot_hub.cpp
+++ AzureIoTHubConnectedServiceLibrary/Resources/CPP/WAC/azure_iot_hub.cpp
@@ -1,8 +1,9 @@
-#include "stdafx.h"
-
 #include <ppltasks.h>
+#include <pplawait.h>
 #include <stdio.h>
 
+using namespace Microsoft::Azure::Devices::Client;
+using namespace Platform;
 using namespace concurrency;
 
 //
@@ -12,21 +13,55 @@ using namespace concurrency;
 // Note: this connection string is specific to the device "$deviceId$". To configure other devices,
 // see information on iothub-explorer at http://aka.ms/iothubgetstartedVSCS
 //
-static const char* connection_string = "HostName=$iotHubUri$;DeviceId=$deviceId$;SharedAccessKey=$deviceKey$";
+static const wchar_t* connection_string = L"HostName=$iotHubUri$;DeviceId=$deviceId$;SharedAccessKey=$deviceKey$";
 
 //
 // To monitor messages sent to device "$deviceId$" use iothub-explorer as follows:
 //    iothub-explorer HostName=$iotHubUri$;SharedAccessKeyName=service;SharedAccessKey=$servicePrimaryKey$ monitor-events "$deviceId$"
 //
 
-
 task<void> send_device_to_cloud_message()
 {
-    auto deviceClient = DeviceClient::CreateFromConnectionString(L"<replace>", TransportType::Http1);
-    byte dataBuffer[] = { 'H', 'e', 'l', 'l', 'o' };
-    auto pbuffer = ref new Platform::Array<byte>(&dataBuffer[0], _countof(dataBuffer));
+    auto deviceClient = DeviceClient::CreateFromConnectionString(ref new Platform::String(connection_string), TransportType::Http1);
+
+    std::string message = "Hello from C++!";
+
+    auto pbuffer = ref new Platform::Array<byte>((unsigned char*)message.data(), message.length());
     auto eventMessage = ref new Message(pbuffer);
     return create_task(deviceClient->SendEventAsync(eventMessage)).then([] {
         OutputDebugString(L"message sent successfully\n");
     });
+}
+
+task<std::string> receive_cloud_to_device_message()
+{
+    auto deviceClient = DeviceClient::CreateFromConnectionString(ref new Platform::String(connection_string), TransportType::Http1);
+
+    while (true)
+    {
+        auto receivedMessage = await deviceClient->ReceiveAsync();
+
+        if (receivedMessage != nullptr)
+        {
+            auto bytes = receivedMessage->GetBytes();
+            auto messageData = bytes->Data;
+            auto len = bytes->Length;
+
+            std::string str((char*)messageData, len);
+
+            await deviceClient->CompleteAsync(receivedMessage);
+
+            return str;
+        }
+
+        //  Note: In this sample, the polling interval is set to 
+        //  10 seconds to enable you to see messages as they are sent.
+        //  To enable an IoT solution to scale, you should extend this 
+        //  interval. For example, to scale to 1 million devices, set 
+        //  the polling interval to 25 minutes.
+        //  For further information, see
+        //  https://azure.microsoft.com/documentation/articles/iot-hub-devguide/#messaging
+
+        await create_task([] { std::this_thread::sleep_for(std::chrono::seconds(10)); });
+    }
 }
--- AzureIoTHubConnectedServiceLibrary/Resources/CPP/WAC/azure_iot_hub.h
+++ AzureIoTHubConnectedServiceLibrary/Resources/CPP/WAC/azure_iot_hub.h
@@ -1,2 +1,3 @@
-// Insert call to this function in your code:
-void send_device_to_cloud_message();
+// Insert call to these functions in your code:
+concurrency::task<void> send_device_to_cloud_message();
+concurrency::task<std::string> receive_cloud_to_device_message();

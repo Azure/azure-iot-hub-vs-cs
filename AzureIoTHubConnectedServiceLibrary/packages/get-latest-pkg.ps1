ad474ea2848acb21bab3a58409f33d1cc0e2fc56
Artur Laksberg <arturl@microsoft.com>
2016-05-31 15:51:18 -0700

    script update

--- AzureIoTHubConnectedServiceLibrary/packages/get-latest-pkg.ps1
+++ AzureIoTHubConnectedServiceLibrary/packages/get-latest-pkg.ps1
@@ -7,6 +7,13 @@ function SplitNameToPkgAndVersion($filename) {
     Write-Host "Error: Unable to extract package name and version from $filename" -foregroundcolor "red"
 }
 
+$target_folder = "new"
+
+if(Test-Path $target_folder)
+{
+    Remove-Item $target_folder -recurse
+}
+
 $packages = @();
 
 ls *.nupkg | % {$_.FullName} | % {
@@ -39,15 +46,31 @@ ls -r *.nupkg | % {$_.FullName} | % {
     }
 }
 
-$target_folder = "new"
-
-if(Test-Path $target_folder)
-{
-    Remove-Item $target_folder -recurse
-}
-
 mkdir $target_folder
 
 $map.Values | % {
     cp $_ $target_folder
+}
+
+# Iterate over the old nupkg files and see if we have a newer version
+
+ls *.nupkg | % {$_.FullName} | % {
+    ($pkg, $version) = SplitNameToPkgAndVersion($_)
+    $search_path = "$target_folder\\*.nupkg"
+    $up_to_date = $True
+    ls $search_path | % {$_.FullName} | % {
+        ($pkg2, $version2) = SplitNameToPkgAndVersion($_)
+        if($pkg -eq $pkg2)
+        {
+            if( (New-Object System.Version($version)) -lt (New-Object System.Version($version2)))
+            {
+                Write-Host "$pkg version $version --> $version2" -foregroundcolor "magenta"
+                $up_to_date = $False
+            }
+        }
+    }
+    if($up_to_date -eq $True)
+    {
+        Write-Host "$pkg is up to date" -foregroundcolor "gray"
+    }
 }
